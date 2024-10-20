d5e60d6659117078f2ff4f213dd0c42f18e7a462
codingsection202 <131967345+codingsection202@users.noreply.github.com>
2024-10-20 12:38:32 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,182 +1,51 @@
-/*!
- * cookie-parser
- * Copyright(c) 2014 TJ Holowaychuk
- * Copyright(c) 2015 Douglas Christopher Wilson
- * MIT Licensed
- */
-
-'use strict'
-
 /**
  * Module dependencies.
- * @private
  */
 
-var cookie = require('cookie')
-var signature = require('cookie-signature')
+var crypto = require('crypto');
 
 /**
- * Module exports.
- * @public
- */
-
-module.exports = cookieParser
-module.exports.JSONCookie = JSONCookie
-module.exports.JSONCookies = JSONCookies
-module.exports.signedCookie = signedCookie
-module.exports.signedCookies = signedCookies
-
-/**
- * Parse Cookie header and populate `req.cookies`
- * with an object keyed by the cookie names.
+ * Sign the given `val` with `secret`.
  *
- * @param {string|array} [secret] A string (or array of strings) representing cookie signing secret(s).
- * @param {Object} [options]
- * @return {Function}
- * @public
+ * @param {String} val
+ * @param {String} secret
+ * @return {String}
+ * @api private
  */
 
-function cookieParser (secret, options) {
-  var secrets = !secret || Array.isArray(secret)
-    ? (secret || [])
-    : [secret]
-
-  return function cookieParser (req, res, next) {
-    if (req.cookies) {
-      return next()
-    }
-
-    var cookies = req.headers.cookie
-
-    req.secret = secrets[0]
-    req.cookies = Object.create(null)
-    req.signedCookies = Object.create(null)
-
-    // no cookies
-    if (!cookies) {
-      return next()
-    }
-
-    req.cookies = cookie.parse(cookies, options)
-
-    // parse signed cookies
-    if (secrets.length !== 0) {
-      req.signedCookies = signedCookies(req.cookies, secrets)
-      req.signedCookies = JSONCookies(req.signedCookies)
-    }
-
-    // parse JSON cookies
-    req.cookies = JSONCookies(req.cookies)
-
-    next()
-  }
-}
+exports.sign = function(val, secret){
+  if ('string' != typeof val) throw new TypeError("Cookie value must be provided as a string.");
+  if ('string' != typeof secret) throw new TypeError("Secret string must be provided.");
+  return val + '.' + crypto
+    .createHmac('sha256', secret)
+    .update(val)
+    .digest('base64')
+    .replace(/\=+$/, '');
+};
 
 /**
- * Parse JSON cookie string.
+ * Unsign and decode the given `val` with `secret`,
+ * returning `false` if the signature is invalid.
  *
- * @param {String} str
- * @return {Object} Parsed object or undefined if not json cookie
- * @public
+ * @param {String} val
+ * @param {String} secret
+ * @return {String|Boolean}
+ * @api private
  */
 
-function JSONCookie (str) {
-  if (typeof str !== 'string' || str.substr(0, 2) !== 'j:') {
-    return undefined
-  }
-
-  try {
-    return JSON.parse(str.slice(2))
-  } catch (err) {
-    return undefined
-  }
-}
+exports.unsign = function(val, secret){
+  if ('string' != typeof val) throw new TypeError("Signed cookie string must be provided.");
+  if ('string' != typeof secret) throw new TypeError("Secret string must be provided.");
+  var str = val.slice(0, val.lastIndexOf('.'))
+    , mac = exports.sign(str, secret);
+  
+  return sha1(mac) == sha1(val) ? str : false;
+};
 
 /**
- * Parse JSON cookies.
- *
- * @param {Object} obj
- * @return {Object}
- * @public
+ * Private
  */
 
-function JSONCookies (obj) {
-  var cookies = Object.keys(obj)
-  var key
-  var val
-
-  for (var i = 0; i < cookies.length; i++) {
-    key = cookies[i]
-    val = JSONCookie(obj[key])
-
-    if (val) {
-      obj[key] = val
-    }
-  }
-
-  return obj
-}
-
-/**
- * Parse a signed cookie string, return the decoded value.
- *
- * @param {String} str signed cookie string
- * @param {string|array} secret
- * @return {String} decoded value
- * @public
- */
-
-function signedCookie (str, secret) {
-  if (typeof str !== 'string') {
-    return undefined
-  }
-
-  if (str.substr(0, 2) !== 's:') {
-    return str
-  }
-
-  var secrets = !secret || Array.isArray(secret)
-    ? (secret || [])
-    : [secret]
-
-  for (var i = 0; i < secrets.length; i++) {
-    var val = signature.unsign(str.slice(2), secrets[i])
-
-    if (val !== false) {
-      return val
-    }
-  }
-
-  return false
-}
-
-/**
- * Parse signed cookies, returning an object containing the decoded key/value
- * pairs, while removing the signed key from obj.
- *
- * @param {Object} obj
- * @param {string|array} secret
- * @return {Object}
- * @public
- */
-
-function signedCookies (obj, secret) {
-  var cookies = Object.keys(obj)
-  var dec
-  var key
-  var ret = Object.create(null)
-  var val
-
-  for (var i = 0; i < cookies.length; i++) {
-    key = cookies[i]
-    val = obj[key]
-    dec = signedCookie(val, secret)
-
-    if (val !== dec) {
-      ret[key] = dec
-      delete obj[key]
-    }
-  }
-
-  return ret
+function sha1(str){
+  return crypto.createHash('sha1').update(str).digest('hex');
 }
