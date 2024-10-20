abdd7196b6cf2b5b1a711fca88440192e70e488e
codingsection202 <131967345+codingsection202@users.noreply.github.com>
2024-10-20 12:50:47 +0530

    Add files via upload

--- Backend/Node_Modules/index.js
+++ Backend/Node_Modules/index.js
@@ -1,78 +1,55 @@
-/*!
- * escape-html
- * Copyright(c) 2012-2013 TJ Holowaychuk
- * Copyright(c) 2015 Andreas Lubbe
- * Copyright(c) 2015 Tiancheng "Timothy" Gu
- * MIT Licensed
- */
-
 'use strict';
 
-/**
- * Module variables.
- * @private
- */
+var $defineProperty = require('../');
 
-var matchHtmlRegExp = /["'&<>]/;
+var test = require('tape');
+var gOPD = require('gopd');
 
-/**
- * Module exports.
- * @public
- */
+test('defineProperty: supported', { skip: !$defineProperty }, function (t) {
+	t.plan(4);
 
-module.exports = escapeHtml;
+	t.equal(typeof $defineProperty, 'function', 'defineProperty is supported');
+	if ($defineProperty && gOPD) { // this `if` check is just to shut TS up
+		var o = { a: 1 };
 
-/**
- * Escape special characters in the given string of html.
- *
- * @param  {string} string The string to escape for inserting into HTML
- * @return {string}
- * @public
- */
+		$defineProperty(o, 'b', { enumerable: true, value: 2 });
+		t.deepEqual(
+			gOPD(o, 'b'),
+			{
+				configurable: false,
+				enumerable: true,
+				value: 2,
+				writable: false
+			},
+			'property descriptor is as expected'
+		);
 
-function escapeHtml(string) {
-  var str = '' + string;
-  var match = matchHtmlRegExp.exec(str);
+		$defineProperty(o, 'c', { enumerable: false, value: 3, writable: true });
+		t.deepEqual(
+			gOPD(o, 'c'),
+			{
+				configurable: false,
+				enumerable: false,
+				value: 3,
+				writable: true
+			},
+			'property descriptor is as expected'
+		);
+	}
 
-  if (!match) {
-    return str;
-  }
+	t.equal($defineProperty, Object.defineProperty, 'defineProperty is Object.defineProperty');
 
-  var escape;
-  var html = '';
-  var index = 0;
-  var lastIndex = 0;
+	t.end();
+});
 
-  for (index = match.index; index < str.length; index++) {
-    switch (str.charCodeAt(index)) {
-      case 34: // "
-        escape = '&quot;';
-        break;
-      case 38: // &
-        escape = '&amp;';
-        break;
-      case 39: // '
-        escape = '&#39;';
-        break;
-      case 60: // <
-        escape = '&lt;';
-        break;
-      case 62: // >
-        escape = '&gt;';
-        break;
-      default:
-        continue;
-    }
+test('defineProperty: not supported', { skip: !!$defineProperty }, function (t) {
+	t.notOk($defineProperty, 'defineProperty is not supported');
 
-    if (lastIndex !== index) {
-      html += str.substring(lastIndex, index);
-    }
+	t.match(
+		typeof $defineProperty,
+		/^(?:undefined|boolean)$/,
+		'`typeof defineProperty` is `undefined` or `boolean`'
+	);
 
-    lastIndex = index + 1;
-    html += escape;
-  }
-
-  return lastIndex !== index
-    ? html + str.substring(lastIndex, index)
-    : html;
-}
+	t.end();
+});
